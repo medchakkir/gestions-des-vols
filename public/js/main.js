562312b53a83ba6fb318abf5fadccb3c7ef96b6e
momed-ali01 <demahomali01@gmail.com>
2025-01-02 14:53:24 +0300

    Refactor header and footer includes for consistency; update navbar functionality and rename files for improved clarity

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector(".navbar-toggle");
-const navbarContent = document.querySelector(".navbar-content");
-
-toggleButton.addEventListener("click", () => {
-  navbarContent.classList.toggle("active");
-});
